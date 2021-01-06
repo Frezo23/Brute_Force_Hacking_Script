ec313cc6d2953b5d29cd4d12195ff9993eb7c4d8
*Frezo* <wilczewski.dominik@gmail.com>
2021-01-06 07:03:07 +0100

    Update Hack-Brutal-V2-string.py

--- Hack-Brutal-V2-string.py
+++ Hack-Brutal-V2-string.py
@@ -1,14 +1,35 @@
 import datetime
 
 tries = 0
-guess = ''
+number = 0
+len_of_pass = 0
+guess = []
+num_of_char_list = []
 
-list_char = 0
+running = 0
+char_pick = 0
 
 characters = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890')
 
 password = input('Put password here: ')
 pass_list = list(password)
 
+for x in range(len(password)):
+    guess.append('')
+    num_of_char_list.append(0)
+
+len_of_pass = len(pass_list) - 1
+
 while True:
-    
+    guess[running] = characters[char_pick]
+    char_pick += 1
+    if char_pick == 62:
+        char_pick = 0
+        if running <= len(pass_list) - 2:
+            running += 1
+        else:
+            running = 0
+    if guess == pass_list:
+        print(guess)
+        print(pass_list)
+            
